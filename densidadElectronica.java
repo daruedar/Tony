921c86a70fe7fa670906eeaac7d8d25eb55479b6
Diego Armando Rueda Robles <daruedar@hotmail.com>
2016-02-01 18:24:46 -0500

    Organizacion del Codigo
    ningun cambio sustancial

--- densidadElectronica.java
+++ densidadElectronica.java
@@ -1,5 +1,4 @@
 package vista;
-
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -14,13 +13,27 @@ public class densidadElectronica extends JPanel implements ActionListener{
 	JTextArea label2,label3,label4,campo3;
 	JTextField campo,campo2,campo8;
 	JLabel rta,label4a;
-	JPanel inf= new JPanel();
-	JButton b1=new JButton("Calcular");
-	JTextField salida= new JTextField(18);
+	JPanel inf;
+	JButton b1;
+	JTextField salida;
 	JPanel ac;
-	JLabel uni= new JLabel("electrones/metro cubico");
+	JLabel uni;
 	
-	public densidadElectronica(){
+	public densidadElectronica(){inicializar();}//Fin del constructor
+
+	@Override
+	public void actionPerformed(ActionEvent e) {if(e.getSource()==b1){salida.setText(""+getDensidadDeElectrones());}}
+	public double getDensidadAtomicaSilicio(){
+		double densidadSilicio=2.33;//g/cm3
+		double aux3=Double.parseDouble(campo8.getText());
+		double factordeConversion=1000000;//cm3 to m3
+		double masaAtomicaSilicio=28.09;//uma
+		double avogadro=0.6023*(Math.pow(10, 24));
+		double sal=densidadSilicio*factordeConversion*(1/masaAtomicaSilicio)*avogadro;
+		//double cas=0;
+		return sal;
+	}//Fin del metodo getDensidadAtomicaSilicio
+	public void inicializar(){
 		enunciado= new JTextArea(5,6);
 		campo= new JTextField(3);
 		campo.setToolTipText("1E14");
@@ -30,17 +43,17 @@ public class densidadElectronica extends JPanel implements ActionListener{
 		enunciado.setText("De cada");
 		campo3= new JTextArea(5,6);
 		campo8= new JTextField(3);
+		salida= new JTextField(18);
+		inf= new JPanel();
+		b1=new JButton("Calcular");
 		campo8.setToolTipText("1");
-		
-		
 		label2= new JTextArea(5,6);
 		label2.setText("atomos de silicio puro");
-		
 		b1.addActionListener(this);
+		uni= new JLabel("electrones/metro cubico");
 		label3= new JTextArea(5,6);
 		label3.setText(", proporcionan ");
 		//campo8.setText();
-		
 		label3= new JTextArea(5,6);
 		label4a= new JLabel("Proporcionan");
 		campo3.setText("electron de conduccion.¿Cual sera la densidad \nde electrones de conduccion n? ");
@@ -60,26 +73,8 @@ public class densidadElectronica extends JPanel implements ActionListener{
 		add(campo3);
 		add(label3);
 		add(inf);
-		add(ac);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==b1){salida.setText(""+getDensidadDeElectrones());}
-		
-	}
-	public double getDensidadAtomicaSilicio(){
-		double densidadSilicio=2.33;//g/cm3
-		
-		double aux3=Double.parseDouble(campo8.getText());
-		double factordeConversion=1000000;//cm3 to m3
-		double masaAtomicaSilicio=28.09;//uma
-		double avogadro=0.6023*(Math.pow(10, 24));
-		double sal=densidadSilicio*factordeConversion*(1/masaAtomicaSilicio)*avogadro;
-		//double cas=0;
-		
-		return sal;
-	}
-	
+		add(ac);	
+	}//Fin del metodo inicializar
+}//Fin de la clase principal densidadElectronica
 	
 	
